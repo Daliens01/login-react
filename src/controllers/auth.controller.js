1b99596c602bfb7d70b25933d63c4775b07f3a57
unknown <gestion.informatica@esiapi.edu.mx>
2024-11-28 16:03:45 -0600

    adding navar

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 import {createAccessToken} from "../libs/jwt.js"
+import jwt from "jsonwebtoken"
+import { SECRET_TOKEN } from "../config.js"
 
 export const register = async (req, res)=>{
     const {username,email,password} = req.body
@@ -73,5 +75,22 @@ export const profile = async (req, res)=>{
       email: userFound.email,
       createAt: userFound.createdAt,
       updateAt: userFound.updatedAt
+    })
+}
+
+export const verifyToken = async (req, res) =>{
+  const {token} = req.cookies
+
+  if(!token) return res.status(401).json({ message: "unauthorized"})
+    jwt.verify(token, SECRET_TOKEN,async(err, user)=>{
+      if(err) return res.status(401).json({ message: "unauthorized"})
+        const userFound = await User.findById(user.id)
+      if(!userFound) return res.status(401).json({ message: "unauthorized"})
+
+        return res.json({
+          id: userFound._id,
+          username: userFound.username,
+          email: userFound.email
+        })
     })
 }
